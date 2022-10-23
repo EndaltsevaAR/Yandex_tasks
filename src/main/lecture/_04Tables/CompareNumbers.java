9825471cc5cd477370c8938270e64204726ed57e
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-10-23 14:14:50 +0300

    lection 4: compare number's digits

---
+++ src/main/lecture/_04Tables/CompareNumbers.java
@@ -0,0 +1,33 @@
+package main.lecture._04Tables;
+
+import java.util.Scanner;
+
+public class CompareNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println(solution(x, y));
+    }
+
+    public static boolean solution(int x, int y) {
+        int[] digits_x = findMapNumber(x);
+        int[] digits_y = findMapNumber(y);
+        for (int i = 0; i < 10; i++) {
+            if (digits_x[i] != digits_y[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int[] findMapNumber(int number) {
+        int[] digits = new int[10];
+        while (number > 0){
+            int digit = number % 10;
+            digits[digit] += 1;
+            number /= 10;
+        }
+        return digits;
+    }
+}
