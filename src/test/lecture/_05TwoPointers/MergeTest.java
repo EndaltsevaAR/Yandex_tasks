aec639fd2abbb1e669c50d9e42efd8ec6d7a06c0
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-10-25 23:58:23 +0300

    lecture 5 two pointers: merge two arrays with test

---
+++ src/test/lecture/_05TwoPointers/MergeTest.java
@@ -0,0 +1,15 @@
+package test.lecture._05TwoPointers;
+import main.lecture._05TwoPointers.Merge;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MergeTest {
+    @Test
+    public void test() {
+        int[] nums1 = {1, 3, 7, 8};
+        int[] nums2 = {2, 4, 5, 6};
+        int[] result = Merge.mergeArrays(nums1, nums2);
+        int[] answer = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        assertArrayEquals(result, answer);
+    }
+}
