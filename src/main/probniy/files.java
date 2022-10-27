588717965b5e78b33ad101ca8e65b0772ca0137f
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-10-28 00:24:34 +0300

    probniy files

---
+++ src/main/probniy/files.java
@@ -0,0 +1,19 @@
+package main.probniy;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class files {
+
+    public static void main(String[] args) throws IOException {
+        PrintWriter writer = new PrintWriter("output.txt");
+        Scanner scanner = new Scanner(new File("input.txt"));
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        writer.print(a+b);
+        writer.close();
+    }
+}
