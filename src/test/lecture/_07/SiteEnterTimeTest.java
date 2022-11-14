12fdace15e9ea46057fcb6fc26254f7135a44a6d
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-11-14 23:47:38 +0300

    lecture 7 second task test

---
+++ src/test/lecture/_07/SiteEnterTimeTest.java
@@ -0,0 +1,18 @@
+package test.lecture._07;
+
+import main.lecture._07.SiteEnterTime;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SiteEnterTimeTest {
+
+    @Test
+    public void test() {
+        int n = 4;
+        int[] enters = new int[]{1, 3, 2, 5};
+        int[] outs = new int[]{9, 6, 3, 7};
+        int result = SiteEnterTime.solution(n, enters, outs);
+        assertEquals(result, 8);
+    }
+}
