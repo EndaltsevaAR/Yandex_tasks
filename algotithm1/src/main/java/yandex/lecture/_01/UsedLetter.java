2fe24d9b8c66a72a795d759615e498486eb1bcba
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-02-29 13:59:52 +0300

    first lecture alg 1

--- algotithm1/src/main/java/yandex/lecture/_01/UsedLetter.java
+++ algotithm1/src/main/java/yandex/lecture/_01/UsedLetter.java
@@ -8,26 +8,26 @@ Description:
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class UsedLetter {
-    // public static void main(String[] args) {
-    // Scanner scanner = new Scanner(System.in);
-    // int answerInt = 0;
-    // char answerChar = ' ';
-    // String line = scanner.nextLine();
-    // Map<Character, Integer> lettersMap = new HashMap<>();
-    // for (int i = 0; i < line.length(); i++) {
-    // if (!lettersMap.containsKey(line.charAt(i))) {
-    // lettersMap.put(line.charAt(i), 0);
-    // }
-    // lettersMap.put(line.charAt(i), lettersMap.get(line.charAt(i)) + 1);
-    // if (lettersMap.get(line.charAt(i)) > answerInt) {
-    // answerInt = lettersMap.get(line.charAt(i));
-    // answerChar = line.charAt(i);
-    // }
-    // }
-    // System.out.println(answerChar);
-    // scanner.close();
-    // }
+    public Character findMostUsedLetter(String line) {
+        if (line == null || line.isEmpty()) {
+            return null;
+        }
+        int answerInt = 0;
+        char answerChar = ' ';
+        Map<Character, Integer> lettersMap = new HashMap<>();
+        for (int i = 0; i < line.length(); i++) {
+            if (!lettersMap.containsKey(line.charAt(i))) {
+                lettersMap.put(line.charAt(i), 0);
+            }
+            lettersMap.put(line.charAt(i), lettersMap.get(line.charAt(i)) + 1);
+            if (lettersMap.get(line.charAt(i)) > answerInt) {
+                answerInt = lettersMap.get(line.charAt(i));
+                answerChar = line.charAt(i);
+            }
+        }
+        System.out.println(answerChar);
+        return answerChar;
+    }
 }
--- algotithm1/src/test/java/yandex/lecture/_01/UsedLetterTest.java
+++ algotithm1/src/test/java/yandex/lecture/_01/UsedLetterTest.java
@@ -1,5 +1,50 @@
 package yandex.lecture._01;
 
-public class UsedLetterTest {
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.Test;
+
+public class UsedLetterTest {
+    private static UsedLetter usedLetter;
+
+    @BeforeAll
+    public static void init() {
+        usedLetter = new UsedLetter();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "aasd", "aaassd, aaa123", "123aaa" })
+    void DiffirentNumbersOfCountlLetter(String line) {
+        assertEquals('a', usedLetter.findMostUsedLetter(line));
+    }
+
+    @Test
+    public void EmptyString() {
+        try {
+            assertNull(usedLetter.findMostUsedLetter(""));
+        } catch (NullPointerException e) {
+            System.err.println("Null is");
+        }
+
+    }
+
+    @Test
+    public void NullString() {
+        try {
+            assertNull(usedLetter.findMostUsedLetter(null));
+        } catch (NullPointerException e) {
+            System.err.println("Null is");
+        }
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "asd", "aszxd, a123" })
+    void EquualNumbersOfCountlLetter(String line) {
+        assertEquals('a', usedLetter.findMostUsedLetter(line));
+    }
 }
