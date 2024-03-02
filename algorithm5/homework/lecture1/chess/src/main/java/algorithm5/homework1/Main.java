a4e9d30ed2b0b7945bd01d44162c3333d623dffd
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-03 01:11:09 +0300

    task4 is solved by 2 try (12 tests)

--- algorithm5/homework/lecture1/chess/src/main/java/algorithm5/homework1/Main.java
+++ algorithm5/homework/lecture1/chess/src/main/java/algorithm5/homework1/Main.java
@@ -1,5 +1,23 @@
 package algorithm5.homework1;
 
+/**
+ * На шахматной доске стоят слоны и ладьи, необходимо посчитать, сколько клеток не бьется ни одной из фигур.
+ * 
+ * Шахматная доска имеет размеры 8 на 8. Ладья бьет все клетки горизонтали и вертикали, проходящих через клетку, 
+ * где она стоит, до первой встретившейся фигуры. Слон бьет все клетки обеих диагоналей, проходящих через клетку, 
+ * где он стоит, до первой встретившейся фигуры.
+ * 
+ * Формат ввода
+ * В первых восьми строках ввода описывается шахматная доска. 
+ * Первые восемь символов каждой из этих строк описывают состояние соответствующей горизонтали: 
+ * символ B (заглавная латинская буква) означает, что в клетке стоит слон, символ R — ладья, символ * — что клетка пуста. 
+ * После описания горизонтали в строке могут идти пробелы, однако длина каждой строки не превышает 250 символов. 
+ * После описания доски в файле могут быть пустые строки.
+ * 
+ * Формат вывода
+ * Выведите количество пустых клеток, которые не бьются ни одной из фигур.
+ */
+
 import java.util.Scanner;
 
 public class Main {
@@ -15,6 +33,7 @@ public class Main {
                 field[i][j] = line.charAt(j);
             }
         }
+
         System.out.println(getUnusedCells(field));
         scanner.close();
     }
@@ -38,7 +57,9 @@ public class Main {
         // horizontal
         int leftCheck = j - 1;
         while (leftCheck >= 0) {
-            if (field[i][leftCheck] == 'R') {
+            if (field[i][leftCheck] == 'B') {
+                break;
+            } else if (field[i][leftCheck] == 'R') {
                 return true;
             }
             leftCheck--;
@@ -46,7 +67,9 @@ public class Main {
 
         int rightCheck = j + 1;
         while (rightCheck < FIELD_LEN) {
-            if (field[i][rightCheck] == 'R') {
+            if (field[i][rightCheck] == 'B') {
+                break;
+            } else if (field[i][rightCheck] == 'R') {
                 return true;
             }
             rightCheck++;
@@ -55,7 +78,9 @@ public class Main {
         // vertical
         int upCheck = i - 1;
         while (upCheck >= 0) {
-            if (field[upCheck][j] == 'R') {
+            if (field[upCheck][j] == 'B') {
+                break;
+            } else if (field[upCheck][j] == 'R') {
                 return true;
             }
             upCheck--;
@@ -63,7 +88,9 @@ public class Main {
 
         int downCheck = i + 1;
         while (downCheck < FIELD_LEN) {
-            if (field[downCheck][j] == 'R') {
+            if (field[downCheck][j] == 'B') {
+                break;
+            } else if (field[downCheck][j] == 'R') {
                 return true;
             }
             downCheck++;
@@ -78,7 +105,9 @@ public class Main {
         int iChechUpLeft = i - 1;
         int jCheckUpLeft = j - 1;
         while (iChechUpLeft >= 0 && jCheckUpLeft >= 0) {
-            if (field[iChechUpLeft][jCheckUpLeft] == 'B') {
+            if (field[iChechUpLeft][jCheckUpLeft] == 'R') {
+                break;
+            } else if (field[iChechUpLeft][jCheckUpLeft] == 'B') {
                 return true;
             }
             iChechUpLeft--;
@@ -89,7 +118,9 @@ public class Main {
         int iChechUpRight = i - 1;
         int jCheckUpLRight = j + 1;
         while (iChechUpRight >= 0 && jCheckUpLRight < FIELD_LEN) {
-            if (field[iChechUpRight][jCheckUpLRight] == 'B') {
+            if (field[iChechUpRight][jCheckUpLRight] == 'R') {
+                break;
+            } else if (field[iChechUpRight][jCheckUpLRight] == 'B') {
                 return true;
             }
             iChechUpRight--;
@@ -100,7 +131,9 @@ public class Main {
         int iChechDownLeft = i + 1;
         int jCheckDownLeft = j - 1;
         while (iChechDownLeft < FIELD_LEN && jCheckDownLeft >= 0) {
-            if (field[iChechDownLeft][jCheckDownLeft] == 'B') {
+            if (field[iChechDownLeft][jCheckDownLeft] == 'R') {
+                break;
+            } else if (field[iChechDownLeft][jCheckDownLeft] == 'B') {
                 return true;
             }
             iChechDownLeft++;
@@ -111,7 +144,9 @@ public class Main {
         int iChechDownRight = i + 1;
         int jCheckDownRight = j + 1;
         while (iChechDownRight < FIELD_LEN && jCheckDownRight < FIELD_LEN) {
-            if (field[iChechDownRight][jCheckDownRight] == 'B') {
+            if (field[iChechDownRight][jCheckDownRight] == 'R') {
+                break;
+            } else if (field[iChechDownRight][jCheckDownRight] == 'B') {
                 return true;
             }
             iChechDownRight++;
--- algorithm5/homework/lecture1/chess/src/test/java/algorithm5/homework1/MainTest.java
+++ algorithm5/homework/lecture1/chess/src/test/java/algorithm5/homework1/MainTest.java
@@ -2,16 +2,13 @@ package algorithm5.homework1;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
 
     @Test
     void contextTestOneRock() {
-        char[][] field = new char[8][8];
+        char[][] field = new char[Main.FIELD_LEN][Main.FIELD_LEN];
         for (int i = 0; i < Main.FIELD_LEN; i++) {
             for (int j = 0; j < Main.FIELD_LEN; j++) {
                 field[i][j] = '*';
@@ -23,7 +20,7 @@ public class MainTest {
 
     @Test
     void contextTestOneBishop() {
-        char[][] field = new char[8][8];
+        char[][] field = new char[Main.FIELD_LEN][Main.FIELD_LEN];
         for (int i = 0; i < Main.FIELD_LEN; i++) {
             for (int j = 0; j < Main.FIELD_LEN; j++) {
                 field[i][j] = '*';
@@ -35,7 +32,7 @@ public class MainTest {
 
     @Test
     void contextTestRockAndBishop() {
-        char[][] field = new char[8][8];
+        char[][] field = new char[Main.FIELD_LEN][Main.FIELD_LEN];
         for (int i = 0; i < Main.FIELD_LEN; i++) {
             for (int j = 0; j < Main.FIELD_LEN; j++) {
                 field[i][j] = '*';
@@ -44,6 +41,22 @@ public class MainTest {
         field[1][1] = 'R';
         field[3][5] = 'B';
         assertEquals(40, Main.getUnusedCells(field));
+    }
+
+    @Test
+    void contextTestRockAndSomeBishops() {
+        char[][] field = new char[Main.FIELD_LEN][Main.FIELD_LEN];
+        for (int i = 0; i < Main.FIELD_LEN; i++) {
+            for (int j = 0; j < Main.FIELD_LEN; j++) {
+                field[i][j] = '*';
+            }
+        }
+        field[2][3] = 'R';
+        field[1][3] = 'B';
+        field[2][2] = 'B';
+        field[2][4] = 'B';
+        field[3][3] = 'B';
+        assertEquals(41, Main.getUnusedCells(field));
     }
 
 }
