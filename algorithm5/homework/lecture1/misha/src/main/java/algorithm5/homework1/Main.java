c4f0eb2e7c1151ad4c3471902889a4c51f8cb5b7
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-04 13:50:17 +0300

    task 6 is solved (22 tests) by 10 try

--- algorithm5/homework/lecture1/misha/src/main/java/algorithm5/homework1/Main.java
+++ algorithm5/homework/lecture1/misha/src/main/java/algorithm5/homework1/Main.java
@@ -1,5 +1,19 @@
 package algorithm5.homework1;
-//
+
+/**
+Миша сидел на занятиях математики в Высшей школе экономики и решал следующую задачу: дано n целых чисел и нужно расставить между ними знаки 
++ и x так, чтобы результат полученного арифметического выражения был нечётным (например, между числами 5, 7, 2, можно расставить 
+арифметические знаки следующим образом: 5 x 7 + 2 = 37). 
+Так как примеры становились все больше и больше, а Миша срочно убегает в гости, от вас требуется написать программу решающую данную задачу.
+
+Формат ввода
+В первой строке содержится единственное число n (2≤n≤10^5). Во второй строке содержится n целых чисел ai, разделённых пробелами (−10^9≤ai≤10^9). 
+Гарантируется, что решение существует.
+
+Формат вывода
+В одной строке выведите n−1 символ + или x, в результате применения которых получается нечётный результат. 
+(Для вывода используйте соответственно знаки «+» (ASCII код—43) и «x» (ASCII код—120), без кавычек).
+ */
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,43 +21,46 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public static final char SUM_SIGN = '+';
+    public static final char MULT_SIGN = 'x';
+
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("input.txt");
         Scanner scanner = new Scanner(file);
         int numberLines = Integer.parseInt(scanner.nextLine());
         String stringDigits = scanner.nextLine();
-        System.out.println(getOperators(numberLines, stringDigits));
+        System.out.println(getOperators(stringDigits));
         scanner.close();
-
     }
 
-    public static String getOperators(int numberLines, String stringDigits) {
-
+    public static String getOperators(String stringDigits) {
         StringBuilder builder = new StringBuilder();
         long[] digits = Arrays.stream(stringDigits.split(" ")).mapToLong(Long::parseLong).toArray();
 
         long sum = digits[0];
         long underSum = 0;
         boolean isMultProcess = false;
-        for (int i = 1; i < digits.length; i++) { // смотрим по сумме слева и числу справа
+        for (int i = 1; i < digits.length; i++) { // we analyse summ at left and right number
             if (digits[i] == 0) {
                 if (sum == 0) {
-                    builder.append('x');
+                    builder.append(MULT_SIGN);
                 } else {
                     if (underSum != 0) {
                         if (Math.abs(sum - underSum) != 0 && Math.abs(sum - underSum) % 2 == 1) {
-                            builder.append('x');
+                            builder.append(MULT_SIGN);
                             sum -= underSum;
                             isMultProcess = false;
                             underSum = 0;
                         } else {
-                            builder.append('+');
+                            builder.append(SUM_SIGN);
                         }
+                    } else {
+                        builder.append(SUM_SIGN);
                     }
                 }
-            } else if (isOdd(digits[i])) { // если справа нечетное
-                if (isOdd(sum)) { // сумма нечетная
-                    builder.append('x');
+            } else if (isOdd(digits[i])) { // number at right is odd
+                if (isOdd(sum)) { // sum is odd
+                    builder.append(MULT_SIGN);
 
                     if (!isMultProcess) {
                         isMultProcess = true;
@@ -55,16 +72,16 @@ public class Main {
                         underSum *= digits[i];
                         sum += underSum;
                     }
-                } else { // сумма четная
-                    builder.append('+');
+                } else { // sum is even
+                    builder.append(SUM_SIGN);
                     if (isMultProcess) {
                         isMultProcess = false;
                         underSum = 0;
                     }
                     sum += digits[i];
                 }
-            } else { // // если справа четное
-                builder.append('+');
+            } else { // right number is even
+                builder.append(SUM_SIGN);
                 if (isMultProcess) {
                     isMultProcess = false;
                     underSum = 0;
@@ -75,32 +92,38 @@ public class Main {
         return builder.toString();
     }
 
-    public static boolean getOperatorsForTest(int numberLines, String stringDigits) {
+    /**
+     * We can not expect particular string becose answers are many, and for testing
+     * we check sum is odd or not
+     */
+    public static boolean getOperatorsForTest(String stringDigits) {
         StringBuilder builder = new StringBuilder();
         long[] digits = Arrays.stream(stringDigits.split(" ")).mapToLong(Long::parseLong).toArray();
 
         long sum = digits[0];
         long underSum = 0;
         boolean isMultProcess = false;
-        for (int i = 1; i < digits.length; i++) { // смотрим по сумме слева и числу справа
+        for (int i = 1; i < digits.length; i++) { // we analyse summ at left and right number
             if (digits[i] == 0) {
                 if (sum == 0) {
-                    builder.append('x');
+                    builder.append(MULT_SIGN);
                 } else {
                     if (underSum != 0) {
                         if (Math.abs(sum - underSum) != 0 && Math.abs(sum - underSum) % 2 == 1) {
-                            builder.append('x');
+                            builder.append(MULT_SIGN);
                             sum -= underSum;
                             isMultProcess = false;
                             underSum = 0;
                         } else {
-                            builder.append('+');
+                            builder.append(SUM_SIGN);
                         }
+                    } else {
+                        builder.append(SUM_SIGN);
                     }
                 }
-            } else if (isOdd(digits[i])) { // если справа нечетное
-                if (isOdd(sum)) { // сумма нечетная
-                    builder.append('x');
+            } else if (isOdd(digits[i])) { // number at right is odd
+                if (isOdd(sum)) { // sum is odd
+                    builder.append(MULT_SIGN);
 
                     if (!isMultProcess) {
                         isMultProcess = true;
@@ -112,16 +135,16 @@ public class Main {
                         underSum *= digits[i];
                         sum += underSum;
                     }
-                } else { // сумма четная
-                    builder.append('+');
+                } else { // sum is even
+                    builder.append(SUM_SIGN);
                     if (isMultProcess) {
                         isMultProcess = false;
                         underSum = 0;
                     }
                     sum += digits[i];
                 }
-            } else { // // если справа четное
-                builder.append('+');
+            } else { // right number is even
+                builder.append(SUM_SIGN);
                 if (isMultProcess) {
                     isMultProcess = false;
                     underSum = 0;
@@ -129,7 +152,6 @@ public class Main {
                 sum += digits[i];
             }
         }
-        System.out.println("sum is " + sum);
         return Math.abs(sum) % 2 == 1;
     }
 
--- algorithm5/homework/lecture1/misha/src/test/java/algorithm5/homework1/MainTest.java
+++ algorithm5/homework/lecture1/misha/src/test/java/algorithm5/homework1/MainTest.java
@@ -8,71 +8,71 @@ public class MainTest {
 
     @Test
     void contextBothOperation() {
-        assertTrue("x+".equals(Main.getOperators(3, "5 7 2")));
+        assertTrue("x+".equals(Main.getOperators("5 7 2")));
     }
 
     @Test
     void contextOneOperation() {
-        assertTrue("+".equals(Main.getOperators(2, "4 -5")));
+        assertTrue("+".equals(Main.getOperators("4 -5")));
     }
 
     void twoOddNumbers() {
-        assertTrue("x".equals(Main.getOperators(2, "1 3")));
+        assertTrue("x".equals(Main.getOperators("1 3")));
     }
 
     @Test
     void firstOddSecondEvenNumbers() {
-        assertTrue("+".equals(Main.getOperators(2, "3 4")));
+        assertTrue("+".equals(Main.getOperators("3 4")));
     }
 
     @Test
     void firstEvenSecondOddNumbers() {
-        assertTrue("+".equals(Main.getOperators(2, "4 3")));
+        assertTrue("+".equals(Main.getOperators("4 3")));
     }
 
     @Test
     void testFuction1() {
-        assertTrue(Main.getOperatorsForTest(4, "2 1 2 3"));
+        assertTrue(Main.getOperatorsForTest("2 1 2 3"));
     }
 
     @Test
     void testFuction2() {
-        assertTrue(Main.getOperatorsForTest(4, "2 2 2 3"));
+        assertTrue(Main.getOperatorsForTest("2 2 2 3"));
     }
 
     @Test
     void testFuction3() {
-        assertTrue(Main.getOperatorsForTest(5, "2 2 2 3 1"));
+        assertTrue(Main.getOperatorsForTest("2 2 2 3 1"));
     }
 
     @Test
     void testFuction4() {
-        assertTrue(Main.getOperatorsForTest(6, "2 2 2 3 1 0"));
+        assertTrue(Main.getOperatorsForTest("2 2 2 3 1 0"));
     }
 
     @Test
     void testFuction5() {
-        assertTrue(Main.getOperatorsForTest(6, "2 0 2 2 3 1 0"));
+        assertTrue(Main.getOperatorsForTest("2 0 2 2 3 1 0"));
     }
 
     @Test
     void testFuctionContext() {
-        assertTrue(Main.getOperatorsForTest(3, "390029247 153996608 -918017777"));
+        assertTrue(Main.getOperatorsForTest("390029247 153996608 -918017777"));
     }
 
     @Test
     void testFuctionContext0() {
-        assertTrue(Main.getOperatorsForTest(10, "0 0 0 0 0 1 1 1 1 1"));
+        assertTrue(Main.getOperatorsForTest("0 0 0 0 0 1 1 1 1 1"));
     }
 
     @Test
     void testFuctionContext01() {
-        assertTrue(Main.getOperatorsForTest(11, "0 0 0 0 0 1 1 1 1 1 1"));
+        assertTrue(Main.getOperatorsForTest("0 0 0 0 0 1 1 1 1 1 1"));
     }
 
     @Test
     void testFuctionContext03() {
-        assertTrue(Main.getOperatorsForTest(12, "0 0 0 0 0 1 1 1 0 1 1 1"));
+        assertTrue(Main.getOperatorsForTest("0 0 0 0 0 1 1 1 0 1 1 1"));
     }
 
 }
