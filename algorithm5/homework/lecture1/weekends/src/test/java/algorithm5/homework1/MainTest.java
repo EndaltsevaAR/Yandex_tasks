5f44608e5e23f4d84fc4bc2ec5fe43cc2bce91cd
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-04 17:19:09 +0300

    task7 first try

--- algorithm5/homework/lecture1/weekends/src/test/java/algorithm5/homework1/MainTest.java
+++ algorithm5/homework/lecture1/weekends/src/test/java/algorithm5/homework1/MainTest.java
@@ -6,4 +6,17 @@ import org.junit.jupiter.api.Test;
 
 public class MainTest {
 
+    @Test
+    void contextTest1() {
+        int nNumberHolidays = 3;
+        int year = 2013;
+        String[] holidays = new String[nNumberHolidays];
+        holidays[0] = "1 January";
+        holidays[1] = "8 January";
+        holidays[2] = "15 January";
+        String firstDayOfWeek = "Tuesday";
+        assertTrue("Monday Tuesday".equals(Main.getRestDayName(year, holidays, firstDayOfWeek)));
+
+    }
+
 }
