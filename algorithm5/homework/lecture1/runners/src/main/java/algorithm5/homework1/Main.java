0d3646fdc632a438b318bc1bb4d5d0d51d319282
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-08 04:56:44 +0300

    task h second solution - 7 test 0k

--- algorithm5/homework/lecture1/runners/src/main/java/algorithm5/homework1/Main.java
+++ algorithm5/homework/lecture1/runners/src/main/java/algorithm5/homework1/Main.java
@@ -1,39 +1,43 @@
 package algorithm5.homework1;
 
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
+    public static final double EPSILON = 0.0000000001;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        long L = scanner.nextLong();
-        long x1 = scanner.nextLong();
-        long v1 = scanner.nextLong();
-        long x2 = scanner.nextLong();
-        long v2 = scanner.nextLong();
+        double L = scanner.nextDouble();
+        double x1 = scanner.nextDouble();
+        double v1 = scanner.nextDouble();
+        double x2 = scanner.nextDouble();
+        double v2 = scanner.nextDouble();
         System.out.println(isBoyMeetings(L, x1, v1, x2, v2));
         scanner.close();
     }
 
-    public static String isBoyMeetings(long L, long x1, long v1, long x2, long v2) {
-        long dx = x1 - x2;
-        long dv = v2 - v1;
+    public static String isBoyMeetings(double L, double x1, double v1, double x2, double v2) {
+        double dx = Math.abs(x1 - x2);
 
-        if (dv == 0) {
-            if (dx == 0) {
-                return "YES\n0";
+        if (v1 < EPSILON && v2 < EPSILON) { // они стоят на месте
+            if (dx < EPSILON || // в одной точке
+                    (Math.abs(L - Math.max(x1, x2)) - Math.abs(0 + Math.min(x1, x2)) < EPSILON)) { // симметрично
+                return "YES\n0.0000000000";
             } else {
                 return "NO";
             }
+        } else if (Math.abs(v1 - v2) < EPSILON) { // они двигаются с одинаковой скоростью
+            double time2 = Math.abs((x2 - x1) / (v1 + v2));
+            return "YES\n" + String.format("%.10f", time2);
         } else {
-            if ((dx % dv == 0) && (dx / dv >= 0) && (dx / dv * v1 + x1 >= 0) && (dx / dv * v2 + x2 >= 0)
-                    && (dx / dv * v1 + x1) % L == 0) {
-                double time = (double) dx / dv;
-                DecimalFormat df = new DecimalFormat("#.#########");
-                return "YES\nd" + df.format(time);
+            if ((x1 < EPSILON || x2 < EPSILON) && (!(x1 < EPSILON && x2 < EPSILON))) { // один стоят на старте
+                double time1 = Math.abs((L - Math.max(x1, x2)) / Math.max(v1, v2));
+                return "YES\n" + String.format("%.10f", time1);
             } else {
-                return "NO";
+                double time1 = Math.abs((x2 - x1) / (v1 - v2));
+                return "YES\n" + String.format("%.10f", time1);
             }
+
         }
     }
 }
--- algorithm5/homework/lecture1/runners/src/test/java/algorithm5/homework1/MainTest.java
+++ algorithm5/homework/lecture1/runners/src/test/java/algorithm5/homework1/MainTest.java
@@ -8,6 +8,37 @@ public class MainTest {
 
     @Test
     void contextTest1() {
-        assertTrue("Yes\n1.0000000000".equals(Main.isBoyMeetings(6, 3, 1, 1, 1)));
+        assertTrue("YES\n1.0000000000".equals(Main.isBoyMeetings(6, 3, 1, 1, 1)));
     }
+
+    @Test
+    void contextTest2() {
+        assertTrue("YES\n0.3000000000".equals(Main.isBoyMeetings(12, 8, 10, 5, 20)));
+    }
+
+    @Test
+    void contextTest3() {
+        assertTrue("YES\n2.0000000000".equals(Main.isBoyMeetings(5, 0, 0, 1, 2)));
+    }
+
+    @Test
+    void contextTest4() {
+        assertTrue("YES\n0.8571428571".equals(Main.isBoyMeetings(10, 7, -3, 1, 4)));
+    }
+
+    @Test
+    void testSpeedsCoordinatesZiro() {
+        assertTrue("YES\n0.0000000000".equals(Main.isBoyMeetings(10, 0, 0, 0, 0)));
+    }
+
+    @Test
+    void testSpeedsZiroCoordinatesDifNotSimmetric() {
+        assertTrue("NO".equals(Main.isBoyMeetings(10, 1, 0, 2, 0)));
+    }
+
+    @Test
+    void testSpeedsZiroCoordinatesZiroButSimmetricIntoL() {
+        assertTrue("YES\n0.0000000000".equals(Main.isBoyMeetings(6, 1, 0, 5, 0)));
+    }
+
 }
