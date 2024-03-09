41ce9d95b73de2cd38da6f78bb59ec8297ce37d1
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-09 22:46:35 +0300

    task f is solved (53 tests)

--- algorithm5/homework/lecture2/f/src/main/java/algorithm5/homework2/Main.java
+++ algorithm5/homework/lecture2/f/src/main/java/algorithm5/homework2/Main.java
@@ -1,5 +1,36 @@
 package algorithm5.homework2;
 
+/**
+Развлекательный телеканал транслирует шоу «Колесо Фортуны». В процессе игры участники шоу крутят большое колесо, разделенное на сектора. 
+В каждом секторе этого колеса записано число. После того как колесо останавливается, специальная стрелка указывает на один из секторов. 
+Число в этом секторе определяет выигрыш игрока.
+
+Юный участник шоу заметил, что колесо в процессе вращения замедляется из-за того, что стрелка задевает за выступы на колесе, находящиеся 
+между секторами. Если колесо вращается с угловой скоростью v градусов в секунду, и стрелка, переходя из сектора X к следующему сектору, 
+задевает за очередной выступ, то текущая угловая скорость движения колеса уменьшается на k градусов в секунду. При этом если v ≤ k, 
+то колесо не может преодолеть препятствие и останавливается. Стрелка в этом случае будет указывать на сектор X.
+
+
+
+Юный участник шоу собирается вращать колесо. Зная порядок секторов на колесе, он хочет заставить колесо вращаться с такой начальной 
+скоростью, чтобы после остановки колеса стрелка указала на как можно большее число. Колесо можно вращать в любом направлении и 
+придавать ему начальную угловую скорость от a до b градусов в секунду.
+
+Требуется написать программу, которая по заданному расположению чисел в секторах, минимальной и максимальной начальной угловой 
+скорости вращения колеса и величине замедления колеса при переходе через границу секторов вычисляет максимальный выигрыш.
+
+Формат ввода
+Первая строка входного файла содержит целое число n — количество секторов колеса (3 ≤ n ≤ 100).
+
+Вторая строка входного файла содержит n положительных целых чисел, каждое из которых не превышает 1000 — числа, записанные 
+в секторах колеса. Числа приведены в порядке следования секторов по часовой стрелке. Изначально стрелка указывает на первое число.
+
+Третья строка содержит три целых числа: a, b и k (1 ≤ a ≤ b ≤ 10^9, 1 ≤ k ≤ 10^9).
+
+Формат вывода
+В выходном файле должно содержаться одно целое число — максимальный выигрыш.
+ */
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -13,9 +44,9 @@ public class Main {
         scanner.close();
     }
 
-    public static long getMaxProfit(int numberCells, String sCells, String speeds) {
+    public static int getMaxProfit(int numberCells, String sCells, String speeds) {
         String[] splitedSCells = sCells.split(" ");
-        long[] cells = new long[numberCells];
+        int[] cells = new int[numberCells];
         for (int i = 0; i < cells.length; i++) {
             cells[i] = Integer.parseInt(splitedSCells[i]);
         }
@@ -26,42 +57,47 @@ public class Main {
         if (bMaxSpeed <= kDeltaSpeed) {
             return cells[0];
         }
-
-        if (bMaxSpeed - aMinSpeed > kDeltaSpeed * numberCells) {
-            return Arrays.stream(cells).max().getAsLong();
+        long speedDif = bMaxSpeed - aMinSpeed;
+        if (speedDif >= kDeltaSpeed * numberCells) {
+            return Arrays.stream(cells).max().getAsInt();
         }
 
-        long maxProfit = 0L;
+        int maxProfit = 0;
+
+        long steps = speedDif / kDeltaSpeed;
         long minMoves = getMovesCount(aMinSpeed, kDeltaSpeed, numberCells);
+        if (steps + minMoves >= numberCells) {
+            minMoves -= ((steps + minMoves) - numberCells + 1);
+            steps = numberCells - 1 - minMoves;
+        }
 
-        for (long speed = aMinSpeed; speed <= bMaxSpeed; speed++) {
-            long moves = getMovesCount(speed, kDeltaSpeed, numberCells);
-            for (long i = minMoves; i <= moves; i++) {
-                long clockwise = cells[(int) i];
-                if (clockwise > maxProfit) {
-                    maxProfit = clockwise;
-                }
-
-                long counterclockwise;
-                if (i == 0) {
-                    counterclockwise = cells[0];
-                } else {
-                    counterclockwise = cells[numberCells - (int) i];
-                }
-                if (counterclockwise > maxProfit) {
-                    maxProfit = counterclockwise;
-                }
+        for (long i = minMoves; i <= minMoves + steps; i++) {
+            int clockwise = cells[(int) i];
+            if (clockwise > maxProfit) {
+                maxProfit = clockwise;
             }
-            minMoves = moves; // пытаюсь уменьшить количество циклов
+
+            int counterclockwise;
+            if (i == 0) {
+                counterclockwise = cells[0];
+            } else {
+                counterclockwise = cells[numberCells - (int) i];
+            }
+            if (counterclockwise > maxProfit) {
+                maxProfit = counterclockwise;
+            }
         }
         return maxProfit;
     }
 
     private static long getMovesCount(long speed, long kDeltaSpeed, long numberCells) {
         long moves = (speed / kDeltaSpeed) % numberCells;
-        if (speed % kDeltaSpeed == 0 && moves > 0) {
-            moves--;
+
+        if (speed % kDeltaSpeed == 0 && moves != 0) {
+            return moves - 1;
+        } else {
+            return moves;
         }
-        return moves;
     }
+
 }
--- algorithm5/homework/lecture2/f/src/test/java/algorithm5/homework2/MainTest.java
+++ algorithm5/homework/lecture2/f/src/test/java/algorithm5/homework2/MainTest.java
@@ -25,11 +25,20 @@ public class MainTest {
         assertEquals(1, Main.getMaxProfit(1, "1 2 3 4 5", "1 1 2"));
     }
 
-    // 8
-    // 952 159 945 463 133 101 767 314
-    // 47 448 28
     @Test
     void contextTest22() {
         assertEquals(952, Main.getMaxProfit(1, "952 159 945 463 133 101 767 314", "47 448 28"));
+    }
+
+    @Test
+    void contextTest11() {
+        assertEquals(468, Main.getMaxProfit(4, "744 43 468 382", "20 48 12"));
+    }
+
+    @Test
+    void contextTest14() {
+        assertEquals(1000, Main.getMaxProfit(34,
+                "56 1000 528 720 895 209 805 65 370 923 541 431 528 778 670 761 794 49 488 171 438 325 57 717 293 847 535 306 398 757 888 56 916 999",
+                "391 901 26"));
     }
 }
