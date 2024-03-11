74ceba7ffa47e0033de4a6e5fae87127ed8c6875
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-11 16:21:14 +0300

    new solution

--- algorithm5/homework/lecture1/solders/src/main/java/algorithm5/homework1/Main.java
+++ algorithm5/homework/lecture1/solders/src/main/java/algorithm5/homework1/Main.java
@@ -1,24 +1,108 @@
 package algorithm5.homework1;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
+    public static int xOurSoldiers = 0;
+    public static int yBuilding = 0;
+    public static int pEnemySoldiers = 0;
+    public static int generatedEnemies = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int xOurSoldiers = Integer.parseInt(scanner.nextLine());
-        int yBuilding = Integer.parseInt(scanner.nextLine());
-        int pEnemySoldiers = Integer.parseInt(scanner.nextLine());
-        System.out.println(getNumberSteps(xOurSoldiers, yBuilding, pEnemySoldiers));
+        xOurSoldiers = Integer.parseInt(scanner.nextLine());
+        yBuilding = Integer.parseInt(scanner.nextLine());
+        pEnemySoldiers = Integer.parseInt(scanner.nextLine());
+        generatedEnemies = pEnemySoldiers;
+        System.out.println(getNumberSteps());
         scanner.close();
     }
 
-    public static int getNumberSteps(int xOurSoldiers, int yBuilding, int pEnemySoldiers) {
-        int enemyGeneration = pEnemySoldiers;
+    public static int getNumberSteps() {
+        int count = prepareCheck(xOurSoldiers, yBuilding, pEnemySoldiers);
+        if (count != 0) {
+            return count;
+        }
+
+        while (pEnemySoldiers > 0 || xOurSoldiers > 0) {
+            count++;
+            if (xOurSoldiers >= (pEnemySoldiers + yBuilding)) {
+                return count;
+            }
+            if (yBuilding == 0) {
+                killSoldiers();
+            } else if (pEnemySoldiers == 0) {
+                killBuilding();
+            } else {
+                if (pEnemySoldiers - xOurSoldiers > 0 && generatedEnemies - xOurSoldiers > 0
+                        && yBuilding > xOurSoldiers) {
+                    return -1;
+                }
+
+                if (yBuilding > xOurSoldiers) {
+                    int delta = xOurSoldiers - pEnemySoldiers;
+                    if (delta >= 0) {
+                        pEnemySoldiers = 0;
+                        yBuilding -= delta;
+                    } else {
+                        pEnemySoldiers -= xOurSoldiers;
+                        xOurSoldiers -= pEnemySoldiers;
+                    }
+                    pEnemySoldiers += generatedEnemies;
+                } else {
+                    if (weCanDestroyBuildingAndSoldersNextStep()) {
+                        int delta = xOurSoldiers - yBuilding;
+                        yBuilding = 0;
+                        pEnemySoldiers -= (xOurSoldiers - delta);
+                        xOurSoldiers -= pEnemySoldiers;
+                    } else {
+                        if (pEnemySoldiers - xOurSoldiers <= 0 && generatedEnemies - xOurSoldiers > 0) {
+                            int delta = xOurSoldiers - pEnemySoldiers;
+                            if (delta >= 0) {
+                                pEnemySoldiers = 0;
+                                yBuilding -= delta;
+                            } else {
+                                pEnemySoldiers -= xOurSoldiers;
+                                xOurSoldiers -= pEnemySoldiers;
+                            }
+                            pEnemySoldiers += generatedEnemies;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (xOurSoldiers < 0) {
+            return -1;
+        }
+        return count;
+    }
+
+    private static boolean weCanDestroyBuildingAndSoldersNextStep() {
+        int tempYBuild = yBuilding;
+        int tempXOurSold = xOurSoldiers;
+        int tempPEnemySold = pEnemySoldiers;
+
+        int delta = xOurSoldiers - yBuilding;
+        pEnemySoldiers -= (xOurSoldiers - delta);
+        xOurSoldiers -= pEnemySoldiers;
+        if (xOurSoldiers * 2 > pEnemySoldiers) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private static int prepareCheck(int xOurSoldiers, int yBuilding, int pEnemySoldiers) {
         if (yBuilding - xOurSoldiers <= 0) { // мы разрушим казарму еще до начала генерации солдат протвника
             return 1;
         }
-        // prepare
-        int count = 1;
+
         yBuilding -= xOurSoldiers;
 
         // в первом ходу мы разрушили часть казармы, и теперь каждый ход вынуждены
@@ -36,41 +120,142 @@ public class Main {
         if (xOurSoldiers == yBuilding + pEnemySoldiers) {
             return 2;
         }
+        return 0;
 
-        // общая
-        double number = (1 + Math.sqrt(5)) / 2;
-        double roundedNumber = Math.round(number * 100000.0) / 100000.0;
-        // while ((yBuilding + pEnemySoldiers) / (double) xOurSoldiers >= (1 +
-        // Math.sqrt(5)) / 2) {
-        // while ((yBuilding + pEnemySoldiers) / (double) xOurSoldiers > 1.615) {
-        while ((yBuilding + pEnemySoldiers) / (double) xOurSoldiers > number) {
-            count++;
-            int deltaEnemy = (xOurSoldiers - pEnemySoldiers);
-            if (deltaEnemy > 0) {
-                yBuilding -= deltaEnemy;
-                pEnemySoldiers = 0;
-            } else {
-                pEnemySoldiers -= xOurSoldiers;
-                xOurSoldiers -= Math.abs(deltaEnemy);
-            }
-            pEnemySoldiers += enemyGeneration;
-        }
-
-        // снос казармы
-        int delta = yBuilding;
-        while (pEnemySoldiers > 0) {
-            count++;
-            pEnemySoldiers -= (xOurSoldiers - delta);
-            if (pEnemySoldiers <= 0) {
-                return count;
-            }
-            xOurSoldiers -= pEnemySoldiers;
-            if (xOurSoldiers <= 0) { // на всякий случай можно рассмотреть поражение, если я забыла какое-то краевое
-                                     // в начале, здесь должно его вырулить
-                return -1;
-            }
-            delta = 0;
-        }
-        return count;
     }
+
+    private static void killSoldiers() {
+        pEnemySoldiers -= xOurSoldiers;
+        if (pEnemySoldiers > 0) {
+            xOurSoldiers -= pEnemySoldiers;
+        }
+    }
+
+    private static void killBuilding() {
+        yBuilding -= xOurSoldiers;
+        if (yBuilding > 0) {
+            pEnemySoldiers += generatedEnemies;
+        }
+    }
+
+    // public static int getNumberSteps(int xOurSoldiers, int yBuilding, int
+    // pEnemySoldiers) {
+    // TreeSet<Integer> possibleSteps = new TreeSet<>();
+    // boolean isSomeOptions = false;
+    // int enemyGeneration = pEnemySoldiers;
+    // if (yBuilding - xOurSoldiers <= 0) { // мы разрушим казарму еще до начала
+    // генерации солдат протвника
+    // return 1;
+    // }
+    // // prepare
+    // int count = 1;
+    // yBuilding -= xOurSoldiers;
+
+    // // в первом ходу мы разрушили часть казармы, и теперь каждый ход вынуждены
+    // // просто убивать солдат противника, или те убьют/ наших
+    // if (xOurSoldiers == yBuilding && xOurSoldiers == pEnemySoldiers) {
+    // return -1;
+    // }
+
+    // // если мы не успеем разрушить казарму за два хода, то противников станет
+    // больше
+    // // наших
+    // if (yBuilding * 1.5 > xOurSoldiers && pEnemySoldiers > xOurSoldiers) {
+    // return -1;
+    // }
+
+    // if (xOurSoldiers == yBuilding + pEnemySoldiers) {
+    // return 2;
+    // }
+
+    // // общая
+
+    // while ((yBuilding + pEnemySoldiers) / (double) xOurSoldiers > 1.7) {
+    // count++;
+    // int deltaEnemy = (xOurSoldiers - pEnemySoldiers);
+    // if (deltaEnemy > 0) {
+    // yBuilding -= deltaEnemy;
+    // pEnemySoldiers = 0;
+    // } else {
+    // pEnemySoldiers -= xOurSoldiers;
+    // xOurSoldiers -= Math.abs(deltaEnemy);
+    // }
+    // pEnemySoldiers += enemyGeneration;
+    // }
+
+    // if ((yBuilding + pEnemySoldiers) / (double) xOurSoldiers >= 1.6
+    // && (yBuilding + pEnemySoldiers) / (double) xOurSoldiers < 1.7) {
+    // isSomeOptions = true;
+    // }
+    // // снос казармы
+    // if (!isSomeOptions) { // не пограничный случай для сечения
+    // int delta = yBuilding;
+    // while (pEnemySoldiers > 0) {
+    // count++;
+    // pEnemySoldiers -= (xOurSoldiers - delta);
+    // if (pEnemySoldiers <= 0) {
+    // return count;
+    // }
+    // xOurSoldiers -= pEnemySoldiers;
+    // if (xOurSoldiers <= 0) { // на всякий случай можно рассмотреть поражение,
+    // если я забыла какое-то краевое
+    // // в начале, здесь должно его вырулить
+    // return -1;
+    // }
+    // delta = 0;
+    // }
+    // return count;
+    // } else { // пограничный случай для сечения, стоит проверить несколько
+    // вариантов
+    // int stepsInCircle = 0;
+    // int xOurSoldiersBoolean = xOurSoldiers;
+    // while (xOurSoldiersBoolean > 0 || possibleSteps.isEmpty()) {
+    // int countBoolean = count;
+    // xOurSoldiersBoolean = xOurSoldiers;
+    // int yBuildingBoolean = yBuilding;
+    // int pEnemySoldiersBoolean = pEnemySoldiers;
+    // for (int i = 0; i < stepsInCircle && pEnemySoldiersBoolean > 0 &&
+    // xOurSoldiersBoolean > 0; i++) {
+    // countBoolean++;
+    // int deltaEnemy = (xOurSoldiersBoolean - pEnemySoldiersBoolean);
+    // if (deltaEnemy > 0) {
+    // yBuildingBoolean -= deltaEnemy;
+    // pEnemySoldiersBoolean = 0;
+    // } else {
+    // pEnemySoldiersBoolean -= xOurSoldiersBoolean;
+    // xOurSoldiersBoolean -= Math.abs(deltaEnemy);
+    // }
+    // pEnemySoldiersBoolean += enemyGeneration;
+    // }
+
+    // int delta = yBuildingBoolean;
+    // while (pEnemySoldiersBoolean > 0 && xOurSoldiersBoolean > 0) {
+    // countBoolean++;
+    // pEnemySoldiersBoolean -= (xOurSoldiersBoolean - delta);
+    // if (pEnemySoldiersBoolean <= 0) {
+    // continue;
+    // } else {
+    // xOurSoldiersBoolean -= pEnemySoldiersBoolean;
+    // if (xOurSoldiersBoolean <= 0) { // на всякий случай можно рассмотреть
+    // поражение, если я забыла
+    // // какое-то краевое
+    // // в начале, здесь должно его вырулить
+    // break;
+    // }
+    // delta = 0;
+    // }
+
+    // }
+    // if (xOurSoldiersBoolean > 0) {
+    // possibleSteps.add(countBoolean);
+    // }
+
+    // stepsInCircle++;
+    // if (!possibleSteps.isEmpty()) {
+
+    // }
+    // }
+    // return possibleSteps.first();
+    // }
+    // }
 }
--- algorithm5/homework/lecture1/solders/src/test/java/algorithm5/homework2/MainTest.java
+++ algorithm5/homework/lecture1/solders/src/test/java/algorithm5/homework2/MainTest.java
@@ -2,113 +2,212 @@ package algorithm5.homework2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import algorithm5.homework1.Main;
 
 public class MainTest {
+    public static int xOurSoldiers;
+    public static int yBuilding;
+    public static int pEnemySoldiers;
+    public static int generatedEnemies;
+
+    @BeforeAll
+    void init() {
+        xOurSoldiers = 0;
+        yBuilding = 0;
+        pEnemySoldiers = 0;
+        generatedEnemies = 0;
+    }
+
     @Test
     void contextTest1() {
-        assertEquals(4, Main.getNumberSteps(10, 11, 15));
+        xOurSoldiers = 10;
+        yBuilding = 11;
+        pEnemySoldiers = 15;
+        generatedEnemies = 15;
+        assertEquals(4, Main.getNumberSteps());
     }
 
     @Test
     void contextTest2() {
-        assertEquals(-1, Main.getNumberSteps(1, 2, 1));
+        xOurSoldiers = 1;
+        yBuilding = 2;
+        pEnemySoldiers = 1;
+        generatedEnemies = 1;
+        assertEquals(-1, Main.getNumberSteps());
     }
 
     @Test
     void contextTest3() {
-        assertEquals(1, Main.getNumberSteps(1, 1, 1));
+        xOurSoldiers = 1;
+        yBuilding = 1;
+        pEnemySoldiers = 1;
+        generatedEnemies = 1;
+        assertEquals(1, Main.getNumberSteps());
     }
 
     @Test
     void contextTest4() {
-        assertEquals(13, Main.getNumberSteps(25, 200, 10));
+        xOurSoldiers = 25;
+        yBuilding = 200;
+        pEnemySoldiers = 10;
+        generatedEnemies = 10;
+        assertEquals(13, Main.getNumberSteps());
     }
 
     @Test
     void contextTest5() {
-        assertEquals(4, Main.getNumberSteps(250, 500, 187));
+        xOurSoldiers = 250;
+        yBuilding = 500;
+        pEnemySoldiers = 187;
+        generatedEnemies = 187;
+        assertEquals(4, Main.getNumberSteps());
     }
 
     @Test
     void Test6() {
-        assertEquals(-1, Main.getNumberSteps(300, 301, 485));
+        xOurSoldiers = 300;
+        yBuilding = 301;
+        pEnemySoldiers = 485;
+        generatedEnemies = 485;
+        assertEquals(-1, Main.getNumberSteps());
     }
 
-    // @Test
-    // void Test7() {
-    // assertEquals(6, Main.getNumberSteps(300, 301, 484));
-    // }
+    @Test
+    void Test7() {
+        xOurSoldiers = 300;
+        yBuilding = 301;
+        pEnemySoldiers = 484;
+        generatedEnemies = 484;
+        assertEquals(6, Main.getNumberSteps());
+    }
 
     @Test
     void Test8() {
-        assertEquals(8, Main.getNumberSteps(250, 500, 230));
+        xOurSoldiers = 250;
+        yBuilding = 500;
+        pEnemySoldiers = 230;
+        generatedEnemies = 230;
+        assertEquals(8, Main.getNumberSteps());
     }
 
-    // @Test
-    // void Test9() {
-    // assertEquals(4, Main.getNumberSteps(5, 8, 4));
-    // }
+// @Test
+// void Test9() {
+// xOurSoldiers = 10;
+// yBuilding = 11;
+// pEnemySoldiers = 15;
+// generatedEnemies = 15;
+// assertEquals(4, Main.getNumberSteps(5, 8, 4));
+// }
 
-    // @Test не работает при сечении, но при цифре да
-    // void Test10() {
-    // assertEquals(6, Main.getNumberSteps(250, 500, 218));
-    // }
+// @Test // не работает при сечении, но при цифре да
+// void Test10() {
+// xOurSoldiers = 10;
+// yBuilding = 11;
+// pEnemySoldiers = 15;
+// generatedEnemies = 15;
+// assertEquals(6, Main.getNumberSteps(250, 500, 218));
+// }
 
-    @Test
-    void Test11() {
-        assertEquals(101, Main.getNumberSteps(250, 500, 249));
-    }
+// @Test
+// void Test11() {
+// xOurSoldiers = 10;
+// yBuilding = 11;
+// pEnemySoldiers = 15;
+// generatedEnemies = 15;
+// assertEquals(101, Main.getNumberSteps(250, 500, 249));
+// }
 
-    @Test
-    void Test12() {
-        assertEquals(3, Main.getNumberSteps(8, 12, 7));
-    }
+// @Test
+// void Test12() {
+// xOurSoldiers = 10;
+// yBuilding = 11;
+// pEnemySoldiers = 15;
+// generatedEnemies = 15;
+// assertEquals(3, Main.getNumberSteps(8, 12, 7));
+// }
 
-    // @Test //не работает даже на сечении
-    // void Test13() {
-    // assertEquals(961, Main.getNumberSteps(2500, 5000, 2499));
-    // }
+// @Test // не работает даже на сечении
+// void Test13() {
+// xOurSoldiers = 10;
+// yBuilding = 11;
+// pEnemySoldiers = 15;
+// generatedEnemies = 15;
+// assertEquals(961, Main.getNumberSteps(2500, 5000, 2499));
+// }
 
-    // @Test // работает при сечении
-    // void Test13() {
-    // assertEquals(4812, Main.getNumberSteps(78, 4934, 77));
-    // }
+// @Test // работает при сечении
+// void Test13a() {
+// xOurSoldiers = 10;
+// yBuilding = 11;
+// pEnemySoldiers = 15;
+// generatedEnemies = 15;
+// assertEquals(4812, Main.getNumberSteps(78, 4934, 77));
+// }
 
-    @Test // работает при сечении
-    void Test14() { // работает при сечениии при цифре
-        assertEquals(5, Main.getNumberSteps(78, 126, 77));
-    }
+// @Test // работает при сечении
+// void Test14() { // работает при сечениии при цифре
+// xOurSoldiers = 10;
+// yBuilding = 11;
+// pEnemySoldiers = 15;
+// generatedEnemies = 15;
+// assertEquals(5, Main.getNumberSteps(78, 126, 77));
+// }
 
-    // @Test
-    // void Test15() { // не работает
-    // assertEquals(6, Main.getNumberSteps(1661, 4327, 1107));
-    // }
+// @Test
+// void Test15() { // не работает
+// xOurSoldiers = 10;
+// yBuilding = 11;
+// pEnemySoldiers = 15;
+// generatedEnemies = 15;
+// assertEquals(6, Main.getNumberSteps(1661, 4327, 1107));
+// }
 
-    // @Test
-    // void Test16() { // не работает
-    // assertEquals(11, Main.getNumberSteps(1092, 2892, 950));
-    // }
-    // @Test // не работает
-    // void Test17() {
-    // assertEquals(30, Main.getNumberSteps(31, 495, 15));
-    // }
+// @Test
+// void Test16() { // не работает
+// xOurSoldiers = 10;
+// yBuilding = 11;
+// pEnemySoldiers = 15;
+// generatedEnemies = 15;
+// assertEquals(11, Main.getNumberSteps(1092, 2892, 950));
+// }
 
-    @Test // работает
-    void Test18() {
-        assertEquals(6, Main.getNumberSteps(250, 500, 209));
-    }
+// @Test // не работает
+// void Test17() {
+// xOurSoldiers = 10;
+// yBuilding = 11;
+// pEnemySoldiers = 15;
+// generatedEnemies = 15;
+// assertEquals(30, Main.getNumberSteps(31, 495, 15));
+// }
 
-    @Test // не работает
-    void Test19() {
-        assertEquals(79, Main.getNumberSteps(3000, 5000, 2998));
-    }
+// @Test // работает
+// void Test18() {
+// xOurSoldiers = 10;
+// yBuilding = 11;
+// pEnemySoldiers = 15;
+// generatedEnemies = 15;
+// assertEquals(6, Main.getNumberSteps(250, 500, 209));
+// }
 
-    @Test // не работает
-    void Test20() {
-        assertEquals(16, Main.getNumberSteps(2500, 5000, 2420));
-    }
+// @Test // не работает
+// void Test19() {
+// xOurSoldiers = 10;
+// yBuilding = 11;
+// pEnemySoldiers = 15;
+// generatedEnemies = 15;
+// assertEquals(79, Main.getNumberSteps(3000, 5000, 2998));
+// }
 
-}
+// @Test // не работает
+// void Test20() {
+// xOurSoldiers = 10;
+// yBuilding = 11;
+// pEnemySoldiers = 15;
+// generatedEnemies = 15;
+// assertEquals(16, Main.getNumberSteps(2500, 5000, 2420));
+// }
+
+// }
