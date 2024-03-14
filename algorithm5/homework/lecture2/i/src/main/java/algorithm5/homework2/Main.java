426e6fdcb4471eb3789429b742b92042f1ea3f4b
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-14 23:32:18 +0300

    task i - first solution

--- algorithm5/homework/lecture2/i/src/main/java/algorithm5/homework2/Main.java
+++ algorithm5/homework/lecture2/i/src/main/java/algorithm5/homework2/Main.java
@@ -2,6 +2,7 @@ package algorithm5.homework2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -14,13 +15,65 @@ public class Main {
         for (int i = 0; i < ships.length; i++) {
             ships[i] = scanner.nextLine();
         }
-        System.out.println(getMinMoves(ships));
+        System.out.println(getMinMoves(numberShips, ships));
         scanner.close();
     }
 
-    public static int getMinMoves(String[] ships) {
-        int countMoves = 0;
+    public static int getMinMoves(int numberShips, String[] ships) {
+        if (numberShips == 1) {
+            return 0;
+        }
+
+        Integer countMoves = 0;
+        int[][] field = initField(numberShips, ships);
+        // movingShipsToOneAtALine(field, countMoves);
+        movingShipsToVertical(field, countMoves);
 
         return countMoves;
+    }
+
+    private static int[][] initField(int numberShips, String[] ships) {
+        int[][] field = new int[numberShips + 2][numberShips + 2];
+
+        // boundaries
+        for (int i = 0; i < field.length; i++) {
+            field[0][i] = -1;
+            field[field.length - 1][i] = -1;
+            field[i][0] = -1;
+            field[i][field.length - 1] = -1;
+        }
+
+        for (String ship : ships) {
+            String[] coordinates = ship.split(" ");
+            field[Integer.parseInt(coordinates[0])][Integer.parseInt(coordinates[1])] = 1;
+        }
+
+        return field;
+    }
+
+    private static void movingShipsToVertical(int[][] field, Integer countMoves) {
+        int[] cols = new int[field.length - 2];
+
+        for (int y = 1; y < field.length - 1; y++) {
+            boolean isLine = false;
+            for (int x = 1; x < field[0].length - 1 && !isLine; x++) {
+                if (field[y][x] == 1) {
+                    cols[y - 1] = x - 1;
+                    isLine = true;
+                }
+            }
+        }
+        int[] sortedCols = cols.clone();
+        Arrays.sort(sortedCols);
+        int median;
+        if (sortedCols.length % 2 == 1) {
+            median = sortedCols[sortedCols.length / 2];
+        } else {
+            median = (sortedCols[sortedCols.length / 2 - 1] + sortedCols[sortedCols.length / 2]) / 2;
+        }
+
+        for (int col : cols) {
+            countMoves += Math.abs(col - median);
+        }
     }
 }
