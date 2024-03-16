8bfa21200cb4e87292aa2dfc2b95aa65b282951b
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-17 01:15:44 +0300

    task j homework2 - first solution

--- algorithm5/homework/lecture2/j/src/main/java/algorithm5/homework2/Main.java
+++ algorithm5/homework/lecture2/j/src/main/java/algorithm5/homework2/Main.java
@@ -1,7 +1,165 @@
 package algorithm5.homework2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("input.txt");
+        Scanner scanner = new Scanner(file);
+        String numbers = scanner.nextLine();
+        int rows = Integer.parseInt(numbers.split(" ")[0]);
+        int cols = Integer.parseInt(numbers.split(" ")[1]);
+        char[][] paint = initArray(rows, cols, scanner);
+
+        scanner.close();
+        if (rows == 1 && cols == 1) {
+            System.out.println("NO");
+            return;
+        }
+        System.out.println(decodePaint(paint));
+    }
+
+    public static char[][] initArray(int rows, int cols, Scanner scanner) {
+        char[][] paint = new char[rows + 2][cols + 2];
+
+        // границы в -1
+        for (int i = 0; i < paint.length; i++) {
+            paint[0][i] = '.';
+            paint[paint.length - 1][i] = '.';
+            paint[i][0] = '.';
+            paint[i][paint.length - 1] = '.';
+        }
+
+        for (int i = 1; i < paint.length - 1; i++) {
+            String[] line = scanner.nextLine().split("");
+            for (int j = 0; j < line.length; j++) {
+                paint[i][j + 1] = line[j].charAt(0);
+            }
+        }
+        return paint;
+    }
+
+    public static String decodePaint(char[][] paint) {
+        List<List<Integer>> figures = findFigures(paint);
+
+        if (figures.size() == 2) {
+            return printYes(figures, paint);
+        } else if (figures.size() == 1) {
+            return printAnswerFromOneFigure(figures, paint);
+        } else {
+            return "NO";
+        }
+    }
+
+    private static List<List<Integer>> findFigures(char[][] paint) {
+        List<List<Integer>> figures = new ArrayList<>();
+
+        int yMin = 0;
+        int xMin = 0;
+        int yMax = 0;
+        int xMax = 0;
+        for (int i = 1; i < paint.length - 1; i++) {
+            for (int j = 1; j < paint[i].length - 1; j++) {
+                if (paint[i][j] == '#') {
+                    List<Integer> figure = new ArrayList<>();
+                    yMin = i;
+                    figure.add(yMin);
+                    xMin = j; // yMin
+                    figure.add(xMin); // xMin
+                    for (int k = j + 1; k < paint[i].length; k++) { // ищем максимум по х
+                        if (paint[i][k] == '.' || paint[i][k] == 'N') {
+                            xMax = k - 1;
+                        }
+                    }
+                    boolean isYMaxFind = false;
+
+                    // смотрим все строчки вниз до поиска
+                    for (int yInFigure = yMin + 1; yInFigure < paint.length && !isYMaxFind; yInFigure++) {
+                        // максимум по y
+                        for (int xInFigure = xMin; xInFigure <= xMax && !isYMaxFind; xInFigure++) {
+                            if (paint[yInFigure][xInFigure] != '#') {
+                                yMax = yInFigure - 1;
+                                isYMaxFind = true;
+                            }
+                        }
+                    }
+                    figure.add(Math.min(yMax, paint.length - 1));
+                    figure.add(Math.min(xMax, paint[i].length - 1));
+
+                    // перекрашивание найденного прямоугольника
+                    for (int yInFigure = yMin; yInFigure <= yMax; yInFigure++) {
+                        for (int xInFigure = xMin; xInFigure <= xMax; xInFigure++) {
+                            paint[yInFigure][xInFigure] = 'N';
+                        }
+                    }
+                    figures.add(figure);
+                }
+            }
+        }
+
+        return figures;
+
+    }
+
+    private static String printYes(List<List<Integer>> figures, char[][] paint) {
+        char letter = 'a';
+        for (List<Integer> figure : figures) {
+            for (int i = figure.get(0); i <= figure.get(2); i++) {
+                for (int j = figure.get(1); j <= figure.get(3); j++) {
+                    paint[i][j] = letter;
+                }
+            }
+            letter++;
+        }
+        return printPaint(paint);
+    }
+
+    private static String printAnswerFromOneFigure(List<List<Integer>> figures, char[][] paint) {
+        List<Integer> figure = figures.get(0);
+        if (figure.get(0).equals(figure.get(2)) && figure.get(1).equals(figure.get(3))) {
+            return "NO";
+        }
+        if ((figure.get(2) - figure.get(0)) - (figure.get(3) - figure.get(1)) > 0) { // длина по У больше
+            for (int i = figure.get(0); i <= figure.get(0) + 1; i++) {
+                for (int j = figure.get(1); j <= figure.get(3); j++) {
+                    paint[i][j] = 'a';
+                }
+            }
+
+            for (int i = figure.get(0) + 1; i <= figure.get(2); i++) {
+                for (int j = figure.get(1); j <= figure.get(3); j++) {
+                    paint[i][j] = 'b';
+                }
+            }
+
+        } else { // делим по х
+            for (int i = figure.get(0); i <= figure.get(2); i++) {
+                for (int j = figure.get(1); j <= figure.get(1) + 1; j++) {
+                    paint[i][j] = 'a';
+                }
+            }
+            for (int i = figure.get(0); i <= figure.get(2); i++) {
+                for (int j = figure.get(1) + 1; j <= figure.get(3); j++) {
+                    paint[i][j] = 'b';
+                }
+            }
+        }
+        return printPaint(paint);
+    }
+
+    private static String printPaint(char[][] paint) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("YES").append("\n");
+        for (int i = 1; i < paint.length - 1; i++) {
+            for (int j = 1; j < paint.length - 1; j++) {
+                builder.append(paint[i][j]);
+            }
+            builder.append("\n");
+        }
+        return builder.deleteCharAt(builder.length() - 1).toString();
     }
 }
--- algorithm5/homework/lecture2/j/src/test/java/algorithm5/homework2/MainTest.java
+++ algorithm5/homework/lecture2/j/src/test/java/algorithm5/homework2/MainTest.java
@@ -1,9 +1,46 @@
 package algorithm5.homework2;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
+    @Test
+    void contextTest1() {
+        String[][] paint = new String[2][1];
+        paint[0][0] = "#";
+        paint[1][0] = ".";
+        assertTrue("NO".equals(Main.decodePaint(paint)));
+    }
 
+    @Test
+    void contextTest2() {
+        char[][] paint = new char[2][2];
+        paint[0][0] = '.';
+        paint[0][1] = '.';
+        paint[1][0] = '#';
+        paint[1][1] = '#';
+        assertTrue("YES\n..\nab".equals(Main.decodePaint(paint)));
+    }
+
+    @Test
+    void contextTest3() {
+        char[][] paint = new char[1][3];
+        paint[0][0] = '#';
+        paint[0][1] = '#';
+        paint[0][2] = '#';
+        assertTrue("YES\nabb".equals(Main.decodePaint(paint)));
+    }
+
+    @Test
+    void contextTest4() {
+        char[][] paint = new char[1][5];
+        paint[0][0] = '#';
+        paint[0][1] = '#';
+        paint[0][2] = '#';
+        paint[0][3] = '#';
+        paint[0][4] = '.';
+
+        assertTrue("YES\nabbb".equals(Main.decodePaint(paint)));
+    }
 }
