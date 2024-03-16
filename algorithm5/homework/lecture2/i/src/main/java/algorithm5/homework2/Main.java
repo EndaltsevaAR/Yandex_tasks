898185589a8f880e51abe1c5e85376b2695eb696
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-16 20:57:25 +0300

    task i - new solution

--- algorithm5/homework/lecture2/i/src/main/java/algorithm5/homework2/Main.java
+++ algorithm5/homework/lecture2/i/src/main/java/algorithm5/homework2/Main.java
@@ -6,59 +6,126 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public static int[][] fields;
+
     public static void main(String[] args) throws FileNotFoundException {
 
         File file = new File("input.txt");
         Scanner scanner = new Scanner(file);
         int numberShips = Integer.parseInt(scanner.nextLine());
         String[] ships = new String[numberShips];
+        fields = new int[numberShips + 2][numberShips + 2];
         for (int i = 0; i < ships.length; i++) {
             ships[i] = scanner.nextLine();
         }
-        System.out.println(getMinMoves(numberShips, ships));
+        System.out.println(getMinMoves(ships));
         scanner.close();
     }
 
-    public static int getMinMoves(int numberShips, String[] ships) {
-        if (numberShips == 1) {
+    public static int getMinMoves(String[] ships) {
+        int countMoves = 0;
+        initField(ships);
+
+        if (isAllColumnHasAllElement()) {
             return 0;
         }
 
-        int countMoves = 0;
-        int[][] field = initField(numberShips, ships);
-        // movingShipsToOneAtALine(field, countMoves);
-        countMoves = movingShipsToVertical(field, countMoves);
+        // Чтобы выйти к медиане, нам надо выйти к состоянию, когда в каждой строчке
+        // было по 1 кораблю
+        countMoves += movingShipsToOneAtALine();
+        countMoves += movingShipsToVertical();
 
         return countMoves;
     }
 
-    private static int[][] initField(int numberShips, String[] ships) {
-        int[][] field = new int[numberShips + 2][numberShips + 2];
-
-        // boundaries
-        for (int i = 0; i < field.length; i++) {
-            field[0][i] = -1;
-            field[field.length - 1][i] = -1;
-            field[i][0] = -1;
-            field[i][field.length - 1] = -1;
+    private static void initField(String[] ships) {
+        // границы в -1
+        for (int i = 0; i < fields.length; i++) {
+            fields[0][i] = -1;
+            fields[fields.length - 1][i] = -1;
+            fields[i][0] = -1;
+            fields[i][fields.length - 1] = -1;
         }
 
         for (String ship : ships) {
             String[] coordinates = ship.split(" ");
-            field[Integer.parseInt(coordinates[0])][Integer.parseInt(coordinates[1])] = 1;
+            fields[Integer.parseInt(coordinates[0])][Integer.parseInt(coordinates[1])] = 1;
         }
-
-        return field;
     }
 
-    private static int movingShipsToVertical(int[][] field, int countMoves) {
-        int countVertical = countMoves;
-        int[] cols = new int[field.length - 2];
+    private static boolean isAllColumnHasAllElement() {
+        boolean isAllColumnHasAllElement = false;
+        for (int i = 1; i < fields[0].length - 1; i++) {
+            if (getColumnSum(i) == fields.length) {
+                return true;
+            }
+        }
+        return isAllColumnHasAllElement;
+    }
 
-        for (int y = 1; y < field.length - 1; y++) {
+    private static int getColumnSum(int columnNumber) {
+        int sum = 0;
+        for (int i = 1; i < fields.length - 1; i++) {
+            sum += fields[i][columnNumber];
+        }
+        return sum;
+    }
+
+    private static int movingShipsToOneAtALine() {
+        int countSteps = 0;
+        while (isAllLineHasOneElement()) {
+            int emptyLinesNumber = 0;
+            for (int i = 1; i < fields.length - 1; i++) {
+                int sumLine = Arrays.stream(fields[i]).sum() + 2;
+                if (sumLine == 1) {
+                    continue;
+                } else if (sumLine == 0) {
+                    emptyLinesNumber++;
+                } else {
+                    int numberShipsToMove = sumLine - 1;
+                    for (int j = 0; j < fields[i].length && numberShipsToMove > 0; j++) {
+                        if (emptyLinesNumber != 0) { // try up
+                            if (fields[i - 1][j] != 1 && fields[i - 1][j] != -1) {
+                                fields[i - 1][j] = 1;
+                                fields[i][j] = 0;
+                                numberShipsToMove--;
+                                emptyLinesNumber--;
+                                countSteps++;
+                            }
+                        } else { // try down
+                            if (fields[i + 1][j] != 1 && fields[i + 1][j] != -1) {
+                                fields[i + 1][j] = 1;
+                                fields[i][j] = 0;
+                                numberShipsToMove--;
+                                countSteps++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return countSteps;
+    }
+
+    private static boolean isAllLineHasOneElement() {
+        boolean isAllLineHasOneElement = true;
+        for (int i = 1; i < fields.length - 1; i++) {
+            if (Arrays.stream(fields[i]).sum() != -1) {
+                return false;
+            }
+        }
+        return isAllLineHasOneElement;
+    }
+
+    private static int movingShipsToVertical() {
+        int countVertical = 0;
+        int[] cols = new int[fields.length - 2];
+
+        for (int y = 1; y < fields.length - 1; y++) {
             boolean isLine = false;
-            for (int x = 1; x < field[0].length - 1 && !isLine; x++) {
-                if (field[y][x] == 1) {
+            for (int x = 1; x < fields[0].length - 1 && !isLine; x++) {
+                if (fields[y][x] == 1) {
                     cols[y - 1] = x - 1;
                     isLine = true;
                 }
--- algorithm5/homework/lecture2/i/src/test/java/algorithm5/homework2/MainTest.java
+++ algorithm5/homework/lecture2/i/src/test/java/algorithm5/homework2/MainTest.java
@@ -1,9 +1,18 @@
 package algorithm5.homework2;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
-
+    @Test
+    void contextTest1() {
+        int n = 3;
+        String[] ships = new String[n];
+        ships[0] = "1 2";
+        ships[1] = "3 3";
+        ships[2] = "1 1";
+        Main.fields = new int[n + 2][n + 2];
+        assertEquals(2, Main.getMinMoves(ships));
+    }
 }
