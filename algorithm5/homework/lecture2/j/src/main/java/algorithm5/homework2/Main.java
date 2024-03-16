1849ab2703eda5d9d05147c99947a0379a03f22a
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-16 22:36:31 +0300

    task j homework2 init

---
+++ algorithm5/homework/lecture2/j/src/main/java/algorithm5/homework2/Main.java
@@ -0,0 +1,7 @@
+package algorithm5.homework2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ algorithm5/homework/lecture2/j/src/test/java/algorithm5/homework2/MainTest.java
@@ -0,0 +1,9 @@
+package algorithm5.homework2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+}
