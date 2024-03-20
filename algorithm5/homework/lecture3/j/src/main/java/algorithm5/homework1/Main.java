e9071a8e59326627884ec8722b4f0d02a04fcc37
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-21 02:33:17 +0300

    task j homework3 is solved by 1 try (74 tests)

--- algorithm5/homework/lecture3/j/src/main/java/algorithm5/homework1/Main.java
+++ algorithm5/homework/lecture3/j/src/main/java/algorithm5/homework1/Main.java
@@ -1,5 +1,43 @@
 package algorithm5.homework1;
 
+/**
+В системе умного дома под управлением голосового помощника Лариса n устройств, соединяющихся между собой по сети LoRaWAN. 
+Устройство номер 1 подключено к интернету и на него было скачано обновление, которое необходимо передать на все устройства.
+
+Сеть LoRaWAN очень медленная, поэтому для распространения протокола был придуман peer-to-peer (P2P) протокол. 
+Файл обновления разбивается на k одинаковых по размеру частей, занумерованных от 1 до k.
+
+Передача части обновления происходит во время таймслотов. Каждый таймслот занимает одну минуту. 
+За один таймслот каждое устройство может получить и передать ровно одну часть обновления. 
+То есть устройство во время таймслота может получать новую часть обновления и передавать уже имеющуюуся у него к началу 
+таймслота часть обновления, или совершать только одно из этих действий, или вообще не осуществлять прием или передачу. 
+После приема части обновления устройство может передавать эту часть обновления другим устройствам в следующих таймслотах.
+
+Перед каждым таймслотом для каждой части обновления определяется, на скольких устройствах сети скачана эта часть. 
+Каждое устройство выбирает отсутствующую на нем часть обновления, которая встречается в сети реже всего. 
+Если таких частей несколько, то выбирается отсутствующая на устройстве часть обновления с наименьшим номером.
+
+После этого устройство делает запрос выбранной части обновления у одного из устройств, на котором такая часть обновления уже скачана. 
+Если таких устройств несколько — выбирается устройство, на котором скачано наименьшее количество частей обновления. 
+Если и таких устройств оказалось несколько — выбирается устройство с минимальным номером.
+
+После того, как все запросы отправлены, каждое устройство выбирает, чей запрос удовлетворить. Устройство A удовлетворяет тот запрос, 
+который поступил от наиболее ценного для A устройства. Ценность устройства B для устройства A определяется как количество частей обновления, 
+ранее полученных устройством A от устройства B. Если на устройство A пришло несколько запросов от одинаково ценных устройств, 
+то удовлетворяется запрос того устройства, на котором меньше всего скачанных частей обновления. Если и таких запросов несколько, 
+то среди них выбирается устройство с наименьшим номером.
+
+Далее начинается новый таймслот. Устройства, чьи запросы удовлетворены, скачивают запрошенную часть обновления, а остальные не скачивают ничего.
+
+Для каждого устройства определите, сколько таймслотов понадобится для скачивания всех частей обновления.
+
+Формат ввода
+Вводится два числа n и k (2 ≤ n ≤ 100, 1 ≤ k ≤ 200).
+
+Формат вывода
+Выведите n-1 число — количество таймслотов, необходимых для скачивания обновления на устройства с номерами от 2 до n.
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -27,13 +65,19 @@ public class Main {
         scanner.close();
     }
 
-    private static String getNumberSteps(int nNumberDevices, int knumberPackages) {
+    public static String getNumberSteps(int nNumberDevices, int knumberPackages) {
         // подготовка
         Set<PackageP2P> packagesP2p = getPackagesP2p(knumberPackages);
         TreeSet<DeviceP2p> devicesP2p = getDevicesP2p(nNumberDevices, packagesP2p);
 
         // основной цикл
         while (!isAllDevicesComplex(devicesP2p)) {
+            // добавляем каждому девайсу по ходу, если они еще не заполнены
+            for (DeviceP2p deviceP2p : devicesP2p) {
+                if (!deviceP2p.notDownloadedPackages.isEmpty()) {
+                    deviceP2p.numberStep++;
+                }
+            }
             Map<PackageP2P, Integer> notDownloadedPackegesMap = getNotDownloadedPackegesMap(devicesP2p);
             Map<DeviceP2p, PackageP2P> candidatesToLoadToDeviceMap = getCandidatesToLoadToDeviceMap(devicesP2p,
                     notDownloadedPackegesMap);
@@ -52,13 +96,6 @@ public class Main {
                 Map<DeviceP2p, Integer> values = pairLoading.getValue().values;
                 DeviceP2p key = pairLoading.getKey();
                 values.put(key, values.getOrDefault(key, 0) + 1);
-
-            }
-
-            // добавляем каждому девайсу по ходу, если они еще не заполнены
-            for (DeviceP2p deviceP2p : devicesP2p) {
-
-                deviceP2p.numberStep++;
             }
         }
 
@@ -88,7 +125,9 @@ public class Main {
         TreeSet<DeviceP2p> devicesP2p = new TreeSet<>();
         for (int i = 0; i < nNumberDevices; i++) {
             DeviceP2p device = new DeviceP2p(i);
-            device.notDownloadedPackages = packagesP2p;
+            for (PackageP2P pack : packagesP2p) {
+                device.notDownloadedPackages.add(pack);
+            }
             devicesP2p.add(device);
         }
         DeviceP2p firstDevice = devicesP2p.first();
@@ -178,16 +217,7 @@ public class Main {
                     return Integer.compare(o1.getKey().idNumberDevice, o2.getKey().idNumberDevice);
                 }
             });
-            Map<DeviceP2p, Integer> sortedSenders = new LinkedHashMap<>();
-            for (Map.Entry<DeviceP2p, Integer> entry : list) {
-                sortedSenders.put(entry.getKey(), entry.getValue());
-            }
-
-            // берем в качестве кандидата загрузчика первый ключ этой мапы
-            for (Map.Entry<DeviceP2p, Integer> sender : sortedSenders.entrySet()) {
-                requestsMap.put(receiving.getKey(), sender.getKey());
-                break;
-            }
+            requestsMap.put(receiving.getKey(), list.get(0).getKey());
         }
         return requestsMap;
     }
@@ -230,6 +260,11 @@ public class Main {
                     if (compareByValue != 0) {
                         return compareByValue;
                     }
+                    compareByValue = Integer.compare(o2.getKey().notDownloadedPackages.size(),
+                            o1.getKey().notDownloadedPackages.size());
+                    if (compareByValue != 0) {
+                        return compareByValue;
+                    }
                     return Integer.compare(o1.getKey().idNumberDevice, o2.getKey().idNumberDevice);
                 }
 
@@ -242,7 +277,7 @@ public class Main {
 
 }
 
-class DeviceP2p {
+class DeviceP2p implements Comparable<DeviceP2p> {
     int idNumberDevice;
     Map<DeviceP2p, Integer> values;
     Set<PackageP2P> notDownloadedPackages;
@@ -254,9 +289,14 @@ class DeviceP2p {
         notDownloadedPackages = new TreeSet<>();
         numberStep = 0;
     }
+
+    @Override
+    public int compareTo(DeviceP2p other) {
+        return Integer.compare(this.idNumberDevice, other.idNumberDevice);
+    }
 }
 
-class PackageP2P {
+class PackageP2P implements Comparable<PackageP2P> {
     int idNumberPackage;
     Set<DeviceP2p> processedDevices;
 
@@ -265,4 +305,8 @@ class PackageP2P {
         processedDevices = new HashSet<>();
     }
 
+    @Override
+    public int compareTo(PackageP2P other) {
+        return Integer.compare(this.idNumberPackage, other.idNumberPackage);
+    }
 }
--- algorithm5/homework/lecture3/j/src/test/java/algorithm5/homework1/MainTest.java
+++ algorithm5/homework/lecture3/j/src/test/java/algorithm5/homework1/MainTest.java
@@ -4,5 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
-
+    @Test
+    void contextTest1() {
+        assertTrue("3 3".equals(Main.getNumberSteps(3, 2)));
+    }
 }
