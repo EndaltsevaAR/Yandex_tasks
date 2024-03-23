51c9717bbf2673a13b5d4cdabe01956c213ad4cf
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-23 20:24:02 +0300

    task a is solved by 7 try (14 tests)

--- algorithm5/homework/lecture4/a/src/main/java/algorithm5/homework4/Main.java
+++ algorithm5/homework/lecture4/a/src/main/java/algorithm5/homework4/Main.java
@@ -1,5 +1,18 @@
 package algorithm5.homework4;
 
+/**
+Дан массив из N целых чисел. Все числа от −10^9 до 10^9.
+Нужно уметь отвечать на запросы вида “Cколько чисел имеют значения от L до R?”.
+
+Формат ввода
+Число N (1≤N≤10^5). Далее N целых чисел.
+Затем число запросов K (1≤K≤10^5).
+Далее K пар чисел L, R (−10^9≤L≤R≤10^9) — собственно запросы.
+
+Формат вывода
+Выведите K чисел — ответы на запросы.
+ */
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,33 +23,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
             int nNumberDigits = Integer.parseInt(reader.readLine());
-            int[] digits = new int[nNumberDigits];
+            long[] digits = new long[nNumberDigits];
             StringTokenizer st = new StringTokenizer(reader.readLine());
             for (int i = 0; i < nNumberDigits; i++) {
-                digits[i] = (Integer.parseInt(st.nextToken()));
+                digits[i] = (Long.parseLong(st.nextToken()));
             }
             int nNumberRequests = Integer.parseInt(reader.readLine());
-            int[][] requests = new int[nNumberRequests][];
+            long[][] requests = new long[nNumberRequests][];
             for (int i = 0; i < nNumberRequests; i++) {
                 StringTokenizer stRequest = new StringTokenizer(reader.readLine());
-                requests[i] = new int[2];
+                requests[i] = new long[2];
                 for (int j = 0; j < requests[i].length; j++) {
-                    requests[i][j] = (Integer.parseInt(stRequest.nextToken()));
+                    requests[i][j] = (Long.parseLong(stRequest.nextToken()));
                 }
             }
             System.out.println(getRequestsAnswer(digits, requests));
         }
     }
 
-    public static String getRequestsAnswer(int[] digits, int[][] requests) {
+    public static String getRequestsAnswer(long[] digits, long[][] requests) {
         Arrays.sort(digits);
         StringBuilder builder = new StringBuilder();
-        for (int[] request : requests) {
-            int leftIndex = binarySearchLeft(digits, request[0]);
-
-            int rightIndex = binarySearchRight(digits, request[1]);
-
-            if (rightIndex < leftIndex || (leftIndex == rightIndex && leftIndex == -1)) {
+        for (int i = 0; i < requests.length; i++) {
+            int leftIndex = binarySearchLeft(digits, requests[i][0]);
+            int rightIndex = binarySearchRight(digits, requests[i][1]);
+            if (leftIndex == rightIndex && leftIndex == -1) {
                 builder.append(0).append(" ");
             } else {
                 if (leftIndex == -1) {
@@ -45,14 +56,19 @@ public class Main {
                 if (rightIndex == -1) {
                     rightIndex = digits.length - 1;
                 }
-                builder.append(rightIndex - leftIndex + 1).append(" ");
+                if (rightIndex < leftIndex) {
+                    builder.append(0).append(" ");
+                } else {
+                    builder.append(rightIndex - leftIndex + 1).append(" ");
+                }
+
             }
 
         }
         return builder.toString().trim();
     }
 
-    private static int binarySearchLeft(int[] digits, int i) {
+    private static int binarySearchLeft(long[] digits, long i) {
         int left = 0;
         int right = digits.length - 1;
 
@@ -67,11 +83,10 @@ public class Main {
                 left = med + 1;
             }
         }
-
         return left;
     }
 
-    private static int binarySearchRight(int[] digits, int i) {
+    private static int binarySearchRight(long[] digits, long i) {
         int left = 0;
         int right = digits.length - 1;
 
@@ -88,5 +103,4 @@ public class Main {
         }
         return right;
     }
-
 }
