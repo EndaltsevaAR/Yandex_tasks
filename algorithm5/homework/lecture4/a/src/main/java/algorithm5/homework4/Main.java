6a67aa5c9d410b59169015df0d422c17c735532f
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-23 16:06:02 +0300

    task a - tests 7

--- algorithm5/homework/lecture4/a/src/main/java/algorithm5/homework4/Main.java
+++ algorithm5/homework/lecture4/a/src/main/java/algorithm5/homework4/Main.java
@@ -3,6 +3,7 @@ package algorithm5.homework4;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -28,20 +29,64 @@ public class Main {
     }
 
     public static String getRequestsAnswer(int[] digits, int[][] requests) {
+        Arrays.sort(digits);
         StringBuilder builder = new StringBuilder();
         for (int[] request : requests) {
-            builder.append(getAnswer(digits, request)).append(" ");
+            int leftIndex = binarySearchLeft(digits, request[0]);
+
+            int rightIndex = binarySearchRight(digits, request[1]);
+
+            if (rightIndex < leftIndex || (leftIndex == rightIndex && leftIndex == -1)) {
+                builder.append(0).append(" ");
+            } else {
+                if (leftIndex == -1) {
+                    leftIndex = 0;
+                }
+                if (rightIndex == -1) {
+                    rightIndex = digits.length - 1;
+                }
+                builder.append(rightIndex - leftIndex + 1).append(" ");
+            }
+
         }
         return builder.toString().trim();
     }
 
-    private static int getAnswer(int[] digits, int[] request) {
-        int count = 0;
-        for (int digit : digits) {
-            if (digit >= request[0] && digit <= request[1]) {
-                count++;
+    private static int binarySearchLeft(int[] digits, int i) {
+        int left = 0;
+        int right = digits.length - 1;
+
+        if (i < digits[left] || i > digits[right]) {
+            return -1;
+        }
+        while (left < right) {
+            int med = (left + right) / 2;
+            if (digits[med] >= i) {
+                right = med;
+            } else {
+                left = med + 1;
             }
         }
-        return count;
+
+        return left;
     }
+
+    private static int binarySearchRight(int[] digits, int i) {
+        int left = 0;
+        int right = digits.length - 1;
+
+        if (i < digits[left] || i > digits[right]) {
+            return -1;
+        }
+        while (left < right) {
+            int med = (left + right + 1) / 2;
+            if (digits[med] <= i) {
+                left = med;
+            } else {
+                right = med - 1;
+            }
+        }
+        return right;
+    }
+
 }
--- algorithm5/homework/lecture4/a/src/test/java/algorithm5/homework4/MainTest.java
+++ algorithm5/homework/lecture4/a/src/test/java/algorithm5/homework4/MainTest.java
@@ -18,4 +18,15 @@ public class MainTest {
 
         assertTrue("5 2 2 0".equals(Main.getRequestsAnswer(digits, requests)));
     }
+
+    @Test
+    void contextTest2() {
+        int[] digits = Arrays.stream("1".split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        int[][] requests = new int[3][];
+        requests[0] = Arrays.stream("1 1".split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        requests[1] = Arrays.stream("-1000000000 -1000000000".split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        requests[2] = Arrays.stream("1000000000 1000000000".split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        assertTrue("1 0 0".equals(Main.getRequestsAnswer(digits, requests)));
+    }
 }
