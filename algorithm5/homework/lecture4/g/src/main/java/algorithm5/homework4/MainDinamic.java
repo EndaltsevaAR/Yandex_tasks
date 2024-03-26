47395409db2372d5408d30736a44c2acdcd0f2b5
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-27 01:41:25 +0300

    task g - dinamic before testing

--- algorithm5/homework/lecture4/g/src/main/java/algorithm5/homework4/MainDinamic.java
+++ algorithm5/homework/lecture4/g/src/main/java/algorithm5/homework4/MainDinamic.java
@@ -32,34 +32,90 @@ public class MainDinamic {
 
                 }
             }
-
             System.out.println(getScale(nHeight, mWidth, field, maxWidth));
         }
     }
 
     public static long getScale(int nHeight, int mLength, boolean[][] field, int maxWidth) {
         int maxScale = Math.min(Math.min(nHeight, mLength) / 3 + 1, maxWidth);
-        boolean[][] initWithOneScale = initWithOneScale(field);
+        boolean[][] scaleField = initWithOneScale(field);
+        int currentScale = 1;
+        while (currentScale <= maxScale) {
+            if (isStop(scaleField)) {
+                return --currentScale;
+            }
+            currentScale++;
+            scaleField = checkNextScale(scaleField, currentScale);
+        }
+        return currentScale;
+    }
 
-        return 0L;
+    private static boolean isStop(boolean[][] scaleField) {
+        int countLine = 0;
+        for (int i = 0; i < scaleField.length; i++) {
+            if (scaleField[i][scaleField[i].length - 1]) {
+                countLine++;
+            } else {
+                if (countLine >= 2) {
+                    return true;
+                }
+                countLine = 0;
+            }
+        }
+        return countLine >= 2;
     }
 
     private static boolean[][] initWithOneScale(boolean[][] field) {
         // последний столбец будет для булеана, есть там 2 элемента или больше или нет
-        boolean[][] initWithOneScale = new boolean[field.length][field[0].length + 1];
+        boolean[][] scaleField = new boolean[field.length][field[0].length + 1];
 
-        for (int i = 1; i < initWithOneScale.length - 1; i++) {
+        for (int i = 1; i < scaleField.length - 1; i++) {
             int numberTrue = 0;
-            for (int j = 1; j < initWithOneScale[0].length - 2; j++) {
+            for (int j = 1; j < scaleField[0].length - 2; j++) {
                 if (field[i][j] && field[i - 1][j] && field[i + 1][j] && field[i][j - 1] && field[i][j + 1]) {
-                    initWithOneScale[i][j] = true;
+                    scaleField[i][j] = true;
                     numberTrue++;
                 }
             }
             if (numberTrue >= 2) {
-                initWithOneScale[i][initWithOneScale[0].length - 1] = true;
+                scaleField[i][scaleField[0].length - 1] = true;
             }
         }
-        return initWithOneScale;
+        return scaleField;
+    }
+
+    private static boolean[][] checkNextScale(boolean[][] scaleField, int currentScale) {
+        boolean[][] scaleFieldNext = new boolean[scaleField.length][scaleField[0].length + 1];
+
+        for (int i = 1; i < scaleField.length - 1; i++) {
+            int numberTrue = 0;
+            for (int j = 1; j < scaleField[0].length - 2; j++) {
+                if (isCross(scaleField, j, i, currentScale)) {
+                    numberTrue++;
+                }
+            }
+            if (numberTrue >= 2) {
+                scaleField[i][scaleField[0].length - 1] = true;
+            }
+        }
+        return scaleField;
+    }
+
+    private static boolean isCross(boolean[][] scaleField, int x, int y, int currentScale) {
+        // проверка границ
+        if (x - currentScale < 0 || x + (currentScale * 2) > scaleField[0].length ||
+                y - currentScale < 0 || y + (currentScale * 2) > scaleField.length) {
+            return false;
+        }
+
+        // проверка квадрата точек со скейлом на 1 меньше
+        if (!(scaleField[y][x + 1 + (currentScale - 2)] && scaleField[y + 1 + (currentScale - 2)][x] &&
+                scaleField[y + 1 + (currentScale - 2)][x + 1 + (currentScale - 2)])) {
+            return false;
+        }
+
+        // проверка доп точек
+
+        return false;
     }
 }
