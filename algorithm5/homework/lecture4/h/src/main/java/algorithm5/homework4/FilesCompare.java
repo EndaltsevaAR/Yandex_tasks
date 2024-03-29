4359634040ec952ba4d917b59c5860ba02b75262
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-29 12:02:32 +0300

    task h - tests 51

---
+++ algorithm5/homework/lecture4/h/src/main/java/algorithm5/homework4/FilesCompare.java
@@ -0,0 +1,33 @@
+// "cwd": "${workspaceFolder}/src/main/java/algorithm5/homework4",
+
+package algorithm5.homework4;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class FilesCompare {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Start!");
+        List<String> mys = new ArrayList<>();
+        List<String> answers = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("output.txt"))) {
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            mys.add(st.nextToken());
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("answer.txt"))) {
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            answers.add(st.nextToken());
+        }
+
+        for (int i = 0; i < answers.size(); i++) {
+            if (!mys.get(i).equals(answers.get(i))) {
+                System.out.println("My answer is " + mys.get(i) + ", answer is " + answers.get(i) + ", index is " + i);
+            }
+        }
+    }
+}
--- algorithm5/homework/lecture4/h/src/main/java/algorithm5/homework4/Main.java
+++ algorithm5/homework/lecture4/h/src/main/java/algorithm5/homework4/Main.java
@@ -1,7 +1,10 @@
 package algorithm5.homework4;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -9,7 +12,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("51"))) {
             StringTokenizer st = new StringTokenizer(reader.readLine());
             int n = Integer.parseInt(st.nextToken());
             Party[] parties = new Party[n];
@@ -64,18 +67,62 @@ public class Main {
         if (partiesWithoutWinners.length == 1) {
             partiesWithoutWinners[0].votes -= left;
         } else if (partiesWithoutWinners.length > 1) {
-            int index = 1;
-            while (left > 0) {
-                long nextAfterHighestVotes = partiesWithoutWinners[index].votes;
-                for (int i = 0; partiesWithoutWinners[i].votes > nextAfterHighestVotes && left > 0; i++) {
-                    long delta = partiesWithoutWinners[i].votes - nextAfterHighestVotes;
+            // int index = 1;
+            // while (left > 0 && index < partiesWithoutWinners.length) { ////// убрать
+            // вторую часть условия
+            // long nextAfterHighestVotes = partiesWithoutWinners[index].votes;
+            // for (int i = 0; partiesWithoutWinners[i].votes > nextAfterHighestVotes &&
+            // left > 0; i++) {
+            // long delta = partiesWithoutWinners[i].votes - nextAfterHighestVotes;
+            // partiesWithoutWinners[i].votes -= Math.min(left, delta);
+            // left -= Math.min(left, delta);
+            // }
+            // index++;
+            // }
+            for (int i = 1; i < partiesWithoutWinners.length; i++) {
+                if (left == 0) {
+                    break;
+                }
+                if (partiesWithoutWinners[i].votes - partiesWithoutWinners[i - 1].votes > 0) {
+                    long delta = partiesWithoutWinners[i].votes - partiesWithoutWinners[i - 1].votes;
                     partiesWithoutWinners[i].votes -= Math.min(left, delta);
                     left -= Math.min(left, delta);
+
+                } else {
+                    break;
                 }
-                index++;
+            }
+            if (left > 0) {
+
+                while (left > 0) {
+                    partiesWithoutWinners[0].votes--;
+                    left--;
+                    for (int i = 1; i < partiesWithoutWinners.length; i++) {
+                        if (left == 0) {
+                            break;
+                        }
+                        if (partiesWithoutWinners[i].votes - partiesWithoutWinners[i - 1].votes > 0) {
+                            long delta = partiesWithoutWinners[i].votes - partiesWithoutWinners[i - 1].votes;
+                            partiesWithoutWinners[i].votes -= delta;
+                            left -= delta;
+
+                        } else {
+                            break;
+                        }
+                    }
+                }
             }
         }
         Arrays.sort(partiesWithoutWinners, Comparator.comparingLong((Party party) -> party.id));
+
+        // для дебага
+        // int ier = -1;
+        // for (int i = 0; i < partiesWithoutWinners.length; i++) {
+        // if (partiesWithoutWinners[i].votes >= winner.votes) {
+        // ier = i;
+        // }
+        // }
+
         for (int i = 0; i < winner.id; i++) {
             builder.append(partiesWithoutWinners[i].votes).append(" ");
         }
@@ -84,7 +131,34 @@ public class Main {
             builder.append(partiesWithoutWinners[i].votes).append(" ");
         }
 
-        return builder.toString().trim();
+        String content = builder.toString().trim();
+        // для дебага
+
+        // // Имя файла, в который мы хотим записать строку
+        // String fileName = "output.txt";
+
+        // try {
+        // // Создаем объект File с указанием имени файла
+        // File file = new File(fileName);
+
+        // // Создаем объект FileWriter с указанием объекта File
+        // FileWriter fw = new FileWriter(file);
+
+        // // Создаем объект BufferedWriter для записи в файл
+        // BufferedWriter bw = new BufferedWriter(fw);
+
+        // // Записываем строку в файл
+        // bw.write(content);
+
+        // // Закрываем BufferedWriter
+        // bw.close();
+
+        // System.out.println("Строка успешно записана в файл " + fileName);
+        // } catch (IOException e) {
+        // // Обработка исключения, если произошла ошибка ввода-вывода
+        // e.printStackTrace();
+        // }
+        return content;
     }
 
     private static boolean isPartyWin(Party winner, long med, Party[] parties, long[] prefixs) {
